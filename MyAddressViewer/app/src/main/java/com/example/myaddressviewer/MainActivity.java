f3c0fb7dfe24d6457cd8134422a2fefe59d030a0
Sriharsha <41952467+sriharsha2000@users.noreply.github.com>
2019-09-21 22:00:47 +0530

    UploadedMyAddressViewer
    MyAddressViewer is an extremely simple app that displays the current address in text.

---
+++ MyAddressViewer/app/src/main/java/com/example/myaddressviewer/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.myaddressviewer;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Message;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+    private FusedLocationProviderClient fusedLocationClient;
+    public TextView address;
+    public Button fetch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        address = findViewById(R.id.address);
+        fetch = findViewById(R.id.fetch);
+        fetch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                address.setText("FETCHING...");
+                loc();
+            }
+        });
+    }
+
+    void loc() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            return;
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            address = findViewById(R.id.address);
+                            address.setText(location.toString());
+                            double latitude = location.getLatitude();
+                            double longitude = location.getLongitude();
+                            LocationAddress locationAddress = new LocationAddress();
+                            locationAddress.getAddressFromLocation(latitude, longitude,
+                                    getApplicationContext(), new GeocoderHandler());
+                        }
+                    }
+                });
+    }
+    private class GeocoderHandler extends Handler {
+        @Override
+        public void handleMessage(Message message) {
+            String locationAddress;
+            switch (message.what) {
+                case 1:
+                    Bundle bundle = message.getData();
+                    locationAddress = bundle.getString("address");
+                    break;
+                default:
+                    locationAddress = null;
+            }
+            address.setText(locationAddress);
+        }
+    }
+}
